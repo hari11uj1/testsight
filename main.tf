610e6ca6602c6b0c4b3e1e2b40a4ee12c12bd793
harish <yarraguntaharish93@gmail.com>
2023-02-10 19:18:35 +0530

    added new resource

--- main.tf
+++ main.tf
@@ -24,3 +24,27 @@ resource "azapi_resource" "purviewresource1" {
     }
   })
 }
+
+resource "azurerm_resource_group" "new_resource101" {
+  name     = "new_resource111"
+  location = "East US 2"
+}
+
+resource "azapi_resource" "purviewresource1" {
+  type      = "Microsoft.Purview/accounts@2021-07-01"
+  name      = "harishperview12"
+  location  = "East US 2"
+  parent_id = azurerm_resource_group.new_resource111.id
+
+  identity {
+    type = "SystemAssigned"
+    #identity_ids = ["SystemAssigned"]
+  }
+  body = jsonencode({
+    properties = {
+      cloudConnectors          = {}
+      managedResourceGroupName = "purviewresource111"
+      publicNetworkAccess      = "Enabled"
+    }
+  })
+}
